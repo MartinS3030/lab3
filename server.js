8c28f63202067f58f22920650888b20c6e3a936a
MartinS3030 <msiu28@my.bcit.ca>
2025-02-02 10:32:53 -0800

    Change server.js to OOP

--- server.js
+++ server.js
@@ -1,45 +1,81 @@
-const { strings } = require('./lang/en/en.js');
-
 const http = require('http');
-const util = require('./modules/utils.js');
 const url = require('url');
+const fs = require('fs');
+const { strings } = require('./lang/en/en.js');
+const util = require('./modules/utils.js');
 
-
-http.createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const pathname = q.pathname;
-    console.log(pathname);
-
-    if (pathname === '/COMP4537/labs/3/getDate/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(`<p style="color:blue;">${util.formatString(strings.greeting, q.query["name"], util.getDate())}</p>`);
-    } else if (pathname === '/COMP4537/labs/3/writeFile/') {
-        const fs = require('fs');
-        const text = q.query["text"];
+class FileHandler {
+    writeFile(res, query) {
+        const text = query["text"];
         fs.appendFile('file.txt', text + '\n', (err) => {
             if (err) {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                res.end(`<h1 style="color:red;">${strings.writeError}</h1>`);
+                this.sendResponse(res, 500, 'text/html', `<h1 style="color:red;">${strings.writeError}</h1>`);
             } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(`<h1 style="color:green;">${strings.writeSuccess}</h1>`);
+                this.sendResponse(res, 200, 'text/html', `<h1 style="color:green;">${strings.writeSuccess}</h1>`);
             }
         });
-    } else if (pathname === '/COMP4537/labs/3/readFile/') {
-        const fs = require('fs');
-        const filename = Object.keys(q.query)[0];
-        console.log(filename);
+    }
+
+    readFile(res, query) {
+        const filename = Object.keys(query)[0];
+        console.log(`Reading file: ${filename}`);
         fs.readFile(filename, (err, data) => {
             if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end(`<h1 style="color:red;">${util.formatString(strings.readError, filename)}</h1>`);
+                this.sendResponse(res, 404, 'text/html', `<h1 style="color:red;">${util.formatString(strings.readError, filename)}</h1>`);
             } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
+                this.sendResponse(res, 200, 'text/html', data);
             }
         });
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end(`<h1 style="color:red;">${strings.notFound}</h1>`);
     }
-}).listen(3000);
+
+    sendResponse(res, statusCode, contentType, content) {
+        res.writeHead(statusCode, { 'Content-Type': contentType });
+        res.end(content);
+    }
+}
+
+class Server {
+    constructor(port) {
+        this.port = port;
+        this.fileHandler = new FileHandler();
+    }
+
+    handleRequest(req, res) {
+        const q = url.parse(req.url, true);
+        const pathname = q.pathname;
+        console.log(`Request for: ${pathname}`);
+
+        switch (pathname) {
+            case '/COMP4537/labs/3/getDate/':
+                this.handleGetDate(res, q.query);
+                break;
+            case '/COMP4537/labs/3/writeFile/':
+                this.fileHandler.writeFile(res, q.query);
+                break;
+            case '/COMP4537/labs/3/readFile/':
+                this.fileHandler.readFile(res, q.query);
+                break;
+            default:
+                this.sendResponse(res, 404, 'text/html', `<h1 style="color:red;">${strings.notFound}</h1>`);
+        }
+    }
+
+    handleGetDate(res, query) {
+        const responseText = `<p style="color:blue;">${util.formatString(strings.greeting, query["name"], util.getDate())}</p>`;
+        this.sendResponse(res, 200, 'text/html', responseText);
+    }
+
+    sendResponse(res, statusCode, contentType, content) {
+        res.writeHead(statusCode, { 'Content-Type': contentType });
+        res.end(content);
+    }
+
+    start() {
+        http.createServer((req, res) => this.handleRequest(req, res)).listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`);
+        });
+    }
+}
+
+const server = new Server(3000);
+server.start();
